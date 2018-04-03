07eef7b6dc307af760e16687da4e5ef747700d0d
mk62109 <37255057+mk62109@users.noreply.github.com>
2018-04-03 15:27:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req, res){
     //articleName == article-one
     //artiles[articleName] == {} content object for article one
-    var articleName = treq.params.articleName;
+    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 

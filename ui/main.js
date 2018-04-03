0d03f9bef47e96436194290049f5fb8345431535
mk62109 <37255057+mk62109@users.noreply.github.com>
2018-04-03 16:05:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
+
+var img = document.getElementById('madi');
+img.onclick = function() {
+  img.style.margininLeft = '100px';  
+};
